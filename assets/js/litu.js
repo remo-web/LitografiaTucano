d1a4255ac2909f6b46e18599494cedbbae3ab609
Raphael <raphaelrpais@gmail.com>
2019-10-08 20:20:31 -0300

    para quem

--- assets/js/litu.js
+++ assets/js/litu.js
@@ -1,12 +1,38 @@
 $(function() {
     
-    $(".litu-menu__nav, #menu-menu").hover(
-        function(){
-            $("#menu-menu").removeClass("litu-hide");
-        }, 
-        function(){
-            $("#menu-menu").addClass("litu-hide");
+//    $(".litu-menu__nav, #menu-nav, .litu-menu__menu").hover(
+//        function(){
+//            $("#menu-nav").removeClass("litu-hide");
+//            $(".litu-menu__menu").removeClass("flipOutX");
+//            $(".litu-menu__menu").addClass("flipInX");
+//        }, 
+//        function(){
+//            $(".litu-menu__menu").removeClass("flipInX");
+//            $(".litu-menu__menu").addClass("flipOutX");
+//        }
+//    );
+    
+//    $( ".litu-menu__nav" ).click( function() {
+//        $("#menu-nav").toggle("litu-hide");
+//        $(".litu-menu__menu").toggle("flipOutX");
+//        $(".litu-menu__menu").toggle("flipInX");
+//    });    
+    
+    $( ".litu-menu__nav, #menu-nav, .litu-menu__menu" ).mouseenter( 
+        function() {
+            $("#menu-nav").removeClass("litu-hide");
+            $(".litu-menu__menu").removeClass("flipOutX").addClass("flipInX");
         }
     );
+    
+    $( ".litu-menu__nav, #menu-nav" ).mouseleave( 
+        function() {
+            $(".litu-menu__menu").removeClass("flipInX").addClass("flipOutX");
+        }
+    );
+    
+    //Altura da imagem do PARA QUEM 
+//    var getHeight = $(".litu-home--para-quem__texto").height();
+//    $(".litu-home--para-quem__foto").height( getHeight );
     
 });
---
+++ plugins/Slick/litu-slick.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.litu-home--para-quem--depoimento').slick({
+        autoplay: true,
+        autoplaySpeed: 6000,
+        dots: true,
+        arrows: false,
+    });
+});
