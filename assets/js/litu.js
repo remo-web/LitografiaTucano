b45f321689d7c210ac32d986d437fcf534ce99e9
Raphael <raphaelrpais@gmail.com>
2019-10-28 17:23:03 -0300

    novo menu
    ainda uma bela bosta

--- assets/js/litu.js
+++ assets/js/litu.js
@@ -2,36 +2,19 @@ $(function() {
     
     feather.replace({ class: 'litu-feather', 'stroke-width': 0.5, 'color': '#2e7d32', })
         
-//    $(".litu-menu__nav, #menu-nav, .litu-menu__menu").hover(
-//        function(){
+// MENU DEPRECATED
+//    $( ".litu-menu__nav, #menu-nav, .litu-menu__menu" ).mouseenter( 
+//        function() {
 //            $("#menu-nav").removeClass("litu-hide");
-//            $(".litu-menu__menu").removeClass("flipOutX");
-//            $(".litu-menu__menu").addClass("flipInX");
-//        }, 
-//        function(){
-//            $(".litu-menu__menu").removeClass("flipInX");
-//            $(".litu-menu__menu").addClass("flipOutX");
+//            $(".litu-menu__menu").removeClass("flipOutX").addClass("flipInX");
 //        }
 //    );
-    
-//    $( ".litu-menu__nav" ).click( function() {
-//        $("#menu-nav").toggle("litu-hide");
-//        $(".litu-menu__menu").toggle("flipOutX");
-//        $(".litu-menu__menu").toggle("flipInX");
-//    });    
-    
-    $( ".litu-menu__nav, #menu-nav, .litu-menu__menu" ).mouseenter( 
-        function() {
-            $("#menu-nav").removeClass("litu-hide");
-            $(".litu-menu__menu").removeClass("flipOutX").addClass("flipInX");
-        }
-    );
-    
-    $( ".litu-menu__nav, #menu-nav" ).mouseleave( 
-        function() {
-            $(".litu-menu__menu").removeClass("flipInX").addClass("flipOutX");
-        }
-    );
+//    
+//    $( ".litu-menu__nav, #menu-nav" ).mouseleave( 
+//        function() {
+//            $(".litu-menu__menu").removeClass("flipInX").addClass("flipOutX");
+//        }
+//    );
     
     // ANO ATUAL
     var date = new Date();
--- plugins/Anime/litu-anime.js
+++ plugins/Anime/litu-anime.js
@@ -6,6 +6,9 @@ textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<
 var textWrapper2 = document.querySelector('.litu-home--head__titulo2 .letters');
 textWrapper2.innerHTML = textWrapper2.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
 
+var textWrapper3 = document.querySelector('.litu-home--head__titulo3 .letters');
+textWrapper3.innerHTML = textWrapper3.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+
 window.onload = function() {
     t1.play()
 };
@@ -16,6 +19,11 @@ var t1 = anime.timeline({
 });
 
 var t2 = anime.timeline({ 
+    loop: false,
+    autoplay: false,
+});
+
+var t3 = anime.timeline({ 
     loop: false,
     autoplay: false,
 });
@@ -87,6 +95,42 @@ t2
     delay: 2000,
     complete: function() {
         $('.litu-home--head__titulo2').attr("style","display: none");
+        $('.litu-home--head__titulo3').attr("style","display: block");
+        t3.restart();
+    },
+});
+    
+t3
+.add({
+    targets: '.litu-home--head__titulo3 .line',
+    scaleY: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 800,
+    begin: function() {
+        $('.litu-home--head__titulo3').attr("style","display: block");
+    },
+}).add({
+    targets: '.litu-home--head__titulo3 .line',
+    translateX: [0, document.querySelector('.litu-home--head__titulo3 .letters').getBoundingClientRect().width + 10],
+    easing: "easeOutExpo",
+    duration: 800,
+    delay: 100
+}).add({
+    targets: '.litu-home--head__titulo3 .letter',
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=775',
+    delay: (el, i) => 34 * (i+1)
+}).add({
+    targets: '.litu-home--head__titulo3',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 2000,
+    complete: function() {
+        $('.litu-home--head__titulo3').attr("style","display: none");
         $('.litu-home--head__titulo1').attr("style","display: block");
         t1.restart();
     },
