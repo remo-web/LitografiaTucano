94543215f0233ce2e9db6fbd4348dc18ad71d9c3
Raphael <raphaelrpais@gmail.com>
2019-11-01 11:52:51 -0300

    dialog orçamento

--- assets/js/mdc.js
+++ assets/js/mdc.js
@@ -1,9 +1,23 @@
 $(function() {
     
+    var btns = document.querySelectorAll('.mdc-button');
+    for (var i = 0, btn; btn = btns[i]; i++) {
+        mdc.ripple.MDCRipple.attachTo(btn);
+    }
+    
+    var btns = document.querySelectorAll('.mdc-list-item');
+    for (var i = 0, btn; btn = btns[i]; i++) {
+        mdc.ripple.MDCRipple.attachTo(btn);
+    }
+    
 //    const dialogs = [].map.call(document.querySelectorAll('.mdc-dialog'), function(el) {
 //        return new MDCDialog(el);
 //    });
     
+    const dialog = new mdc.dialog.MDCDialog(document.querySelector('.mdc-dialog'));
+    document.querySelector('.litu-orcamento--btn').addEventListener('click', function (evt) {
+        dialog.open();
+    })
     mdc.dialog.MDCDialog.attachTo(document.querySelector('.mdc-dialog'));
 
 });
