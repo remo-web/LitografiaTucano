4452f11e288028df314ef867306e8ce15abe737d
Raphael <raphaelrpais@gmail.com>
2019-11-25 14:22:48 -0300

    portfolio

--- assets/js/litu.js
+++ assets/js/litu.js
@@ -3,6 +3,8 @@ $(function() {
     
     // Feather Icon
     feather.replace({ class: 'litu-feather', 'stroke-width': 0.5, 'color': '#2e7d32', })
+    
+    
         
     // ANO ATUAL
     var date = new Date();
