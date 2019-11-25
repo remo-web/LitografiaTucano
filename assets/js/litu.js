10368abb1efe4a33c2674d79433639313b442e47
Raphael <raphaelrpais@gmail.com>
2019-11-25 00:07:44 -0300

    seguindo
    loucamente

--- assets/js/litu.js
+++ assets/js/litu.js
@@ -8,5 +8,6 @@ $(function() {
     var date = new Date();
     var year = date.getFullYear();
     document.getElementById("year").innerHTML = year;
+    document.getElementById("quemsomos-year").innerHTML = year;
 
 });
