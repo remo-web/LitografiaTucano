65e2e36942a1b518de759d8d2a076598fe2b076a
Raphael <raphaelrpais@gmail.com>
2020-01-28 16:46:52 -0300

    menu

--- assets/js/litu.js
+++ assets/js/litu.js
@@ -1,6 +1,18 @@
 
 $(function() {
     
+    $( ".litu-menu" ).hover( function() {
+        $( ".litu-menu__menu .mdc-typography--button").toggleClass( "litu-hide" );
+        $( ".mdc-list-item__graphic" ).toggleClass( "mdc-list-item__graphic__hover" );
+        $( this ).toggleClass( "litu-menu__hover mdc-elevation--z2 mdc-elevation--z6" );
+        $( ".litu-menu--social a" ).toggleClass( "litu-opacity" );
+    }, function() {
+        $( ".litu-menu__menu .mdc-typography--button").toggleClass( "litu-hide" );
+        $( ".mdc-list-item__graphic" ).toggleClass( "mdc-list-item__graphic__hover" );
+        $( this ).toggleClass( "litu-menu__hover mdc-elevation--z2 mdc-elevation--z6" );
+        $( ".litu-menu--social a" ).toggleClass( "litu-opacity" );
+    });
+    
     // ANO ATUAL
     var date = new Date();
     var year = date.getFullYear();
