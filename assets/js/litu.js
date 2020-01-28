a8a9ecb00306a3903a11c1b1173fa9f34932cca6
Raphael <raphaelrpais@gmail.com>
2020-01-28 20:27:58 -0300

    menu

--- assets/js/litu.js
+++ assets/js/litu.js
@@ -1,3 +1,18 @@
+//$(window).scroll(function() {
+//    if ($(this).scrollTop() === 0) { // this refers to window
+//        $( ".litu-menu__menu .mdc-typography--button").removeClass( "litu-hide" );
+//        $( ".mdc-list-item__graphic" ).addClass( "mdc-list-item__graphic__hover" );
+//        $( ".litu-menu" ).addClass( "litu-menu__hover" );
+//        $( ".litu-menu--social a" ).removeClass( "litu-opacity" );
+//        $( ".litu-menu .mdc-list-item").addClass( "mdc-list-item__hover" );
+//    } if ($(this).scrollTop() > 0) {
+//        $( ".litu-menu__menu .mdc-typography--button").toggleClass( "litu-hide" );
+//        $( ".mdc-list-item__graphic" ).toggleClass( "mdc-list-item__graphic__hover" );
+//        $( ".litu-menu" ).toggleClass( "litu-menu__hover mdc-elevation--z2 mdc-elevation--z6" );
+//        $( ".litu-menu--social a" ).toggleClass( "litu-opacity" );
+//        $( ".litu-menu .mdc-list-item").toggleClass( "mdc-list-item__hover" );
+//    }
+//});
 
 $(function() {
     
